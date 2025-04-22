2277f8870eba5722f2196996fccfc99af1932e83
ethanjiang-1 <146405629+ethanjiang-1@users.noreply.github.com>
2025-04-22 02:53:45 +0000

    finished

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -4,19 +4,38 @@ class ZebraController < ApplicationController
   end
   
   def giraffe
-    @random_move = ["rock", "paper", "scissors"].sample
+    @user_move = "rock"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    @result = eval_move(@user_move, @computer_move)
 
     render({:template => "game_templates/play_rock"})
   end
 
   def elephant
-    @random_move = ["rock", "paper", "scissors"].sample
+    @user_move = "paper"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    @result = eval_move(@user_move, @computer_move)
 
     render({:template => "game_templates/play_paper"})
   end
+
   def lion
-    @random_move = ["rock", "paper", "scissors"].sample
+    @user_move = "scissors"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    @result = eval_move(@user_move, @computer_move)
 
     render({:template => "game_templates/play_scissors"})
+  end
+
+  def eval_move(user_move, computer_move)
+    if user_move == computer_move
+      return "We tied!"
+    elsif (user_move == "rock" && computer_move == "scissors") ||
+          (user_move == "paper" && computer_move == "rock") ||
+          (user_move == "scissors" && computer_move == "paper")
+      return "We won!"
+    else
+      return "We lost!"
+    end
   end
 end
